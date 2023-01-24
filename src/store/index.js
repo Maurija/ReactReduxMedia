74f371c46fea9fd2eb7b4a302a6e10c0a3c9c32f
Mauro <mauroevangelista08@gmail.com>
2023-01-24 12:22:19 -0300

    App with Async thunks

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { usersReducer } from "./slices/usersSlice";
+import { configureStore } from "@reduxjs/toolkit";
+
+
+export const store = configureStore({
+    reducer: {
+        users: usersReducer
+    }
+});
+
+
+export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser"
+export * from "./thunks/removeUser"
